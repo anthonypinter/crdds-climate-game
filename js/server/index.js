e8ffa9b99cb5c298233cd23ae3499966c6690f1c
Anthony Pinter <anthony.pinter@colorado.edu>
2022-10-11 11:52:34 -0600

    rebuilding back end continues

--- js/server/index.js
+++ js/server/index.js
@@ -21,9 +21,11 @@ app.use((req, res, next) => {
 });
 
   
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 app.get('/', async (req, res) => {
   let data = JSON.parse(await fs.readFile(dataFile, 'utf-8'));
